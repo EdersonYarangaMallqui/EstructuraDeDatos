2cd0f3c16ac757f5718cd13206589ac7a11f8afe
JoseMerkado <105125496+JoseMerkado@users.noreply.github.com>
2024-04-19 12:11:00 -0500

    cambio 3

---
+++ CursoEstructuraDatos2024/Laboratorio2/Program.cs
@@ -0,0 +1,11 @@
+﻿// programa que sume los n primeros numeros
+
+var n = 50;
+var suma = 0;
+
+for (var i = 1; i <= n; i++)
+{
+    suma = suma + i;
+}
+
+Console.WriteLine($"La suma de los {n} primeros números es: {suma}");
