f1b1ad475bd0bd4bd45df7519280a8a7edf0d768
EdersonYaranga <105125496+EdersonYaranga@users.noreply.github.com>
2024-05-17 23:58:30 -0500

    Laboratorio 8
    LISTAS

---
+++ Laboratorio8/Laboratorio8/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Laboratorio8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            button5 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(26, 594);
+            button1.Name = "button1";
+            button1.Size = new Size(169, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Mostrar Resultado";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(26, 254);
+            button2.Name = "button2";
+            button2.Size = new Size(144, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Agregar Numero";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(26, 216);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(144, 23);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(215, 216);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(167, 23);
+            textBox2.TabIndex = 4;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(225, 594);
+            button3.Name = "button3";
+            button3.Size = new Size(70, 23);
+            button3.TabIndex = 5;
+            button3.Text = "Limpiar";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(225, 254);
+            button4.Name = "button4";
+            button4.Size = new Size(139, 23);
+            button4.TabIndex = 6;
+            button4.Text = "Agregar Palabra";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(26, 47);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(144, 163);
+            textBox3.TabIndex = 7;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(26, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(99, 15);
+            label1.TabIndex = 8;
+            label1.Text = "Lista de Números";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(225, 29);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 15);
+            label2.TabIndex = 9;
+            label2.Text = "Lista de Palabras";
+            label2.Click += label2_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(215, 47);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(167, 163);
+            textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(26, 293);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(356, 295);
+            textBox5.TabIndex = 11;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(304, 594);
+            button5.Name = "button5";
+            button5.Size = new Size(78, 23);
+            button5.TabIndex = 12;
+            button5.Text = "Salir";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(398, 641);
+            Controls.Add(button5);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Button button5;
+    }
+}
---
+++ Laboratorio8/Laboratorio8/Form1.cs
@@ -0,0 +1,49 @@
+namespace Laboratorio8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> lista;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(listaSemana.Count().ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
